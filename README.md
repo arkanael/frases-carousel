137e03c488d6ac172487c174b5932e012a4c8a79
Luiz Guilherme Bandeira <arkanael@gmail.com>
2021-03-30 22:23:57 -0300

    Adição de frases

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Carrossel de avaliações
+# Carrossel de frases
 
-## Carrossel de avaliações com um botão que gera avaliações aleatórias.
+## Carrossel com frases com um botão que gera avaliações aleatórias.
 
-## Este é um bom recurso para ter em um site de comércio eletrônico para exibir avaliações de clientes ou um portfólio pessoal para exibir avaliações de clientes.
+## Este é um bom recurso para ter em um site de comércio eletrônico para exibir avaliações de clientes ou um portfólio pessoal para exibir mensgens de clientes.
 
 ## Principais conceitos abordados:
 
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,48 +2,93 @@
 const reviews = [
     {
         id: 1,
-        name: "susan smith",
-        job: "web developer",
+        name: "Steve Jobs",
+        job: "Ex-presidente da Apple Inc.",
         img:
-            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+            "https://s.ebiografia.com/assets/img/authors/st/ev/steve-jobs-2-l.jpg",
         text:
-            "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
+            "“Ser o homem mais rico do cemitério não me interessa...Ir para a cama à noite dizendo que fizemos algo maravilhoso, isso é o que importa para mim”.",
+    },   
     {
         id: 2,
-        name: "anna johnson",
-        job: "web designer",
+        name: "Bill Gates",
+        job: "Fundador da Microsoft",
         img:
-            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+            "https://cdn-istoedinheiro-ssl.akamaized.net/wp-content/uploads/sites/17/2021/02/bill-gates-e1613016156235.jpg",
         text:
-            "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+            "“A paciência é um dos elementos chave para o sucesso”.",
     },
     {
         id: 3,
-        name: "peter jones",
-        job: "intern",
+        name: "Elon Musk",
+        job: "CEO da Tesla",
         img:
-            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+            "https://img.olhardigital.com.br/wp-content/uploads/2021/01/shutterstock_1866780067-1.jpg",
         text:
-            "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+            "“As pessoas trabalham melhor quando sabem qual é o objetivo e o porquê. É importante que as pessoas estejam ansiosas para vir trabalhar de manhã e gostem de trabalhar”.",
     },
     {
         id: 4,
-        name: "bill anderson",
-        job: "the boss",
+        name: "Jeff Bezos",
+        job: "CEO da Amazon",
         img:
-            "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+            "https://i0.wp.com/assets.propmark.com.br/uploads/2019/09/jeff-bezos-2.jpg?resize=768%2C432&ssl=1",
         text:
-            "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+            "“A melhor maneira de escapar de uma caixa é inventando seu próprio caminho para fora”.",
     },
     {
         id: 5,
-        name: "Luiz Guilherme Bandeira",
-        job: "WEB DEVELOPER",
+        name: "Jack Ma",
+        job: "Fundador da Alibaba",
         img:
-            "./assets/img/eu.jpg",
+            "https://s2.glbimg.com/Q-vFS5ZXA5HzbIzx7QpBQZZS4WY=/0x0:1353x1789/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2020/d/D/SxACvqTh2WAHb1884S8w/foto29emp-101-alibaba-b7.jpg",
         text:
-            "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+            "“Se você tem uma mentalidade diferente, você terá um resultado diferente: se você fez escolhas diferentes de seus colegas, sua vida será, então, diferente de seus pares”.",
+    },
+    {
+        id: 6,
+        name: "Steve Jobs",
+        job: "Ex-presidente da Apple Inc.",
+        img:
+            "https://s.ebiografia.com/assets/img/authors/st/ev/steve-jobs-2-l.jpg",
+        text:
+            "“Você pode encarar um erro como uma besteira a ser esquecida ou como um resultado que aponta uma nova direção”.",
+    },
+    {
+        id: 7,
+        name: "Bill Gates",
+        job: "Fundador da Microsoft",
+        img:
+            "https://cdn-istoedinheiro-ssl.akamaized.net/wp-content/uploads/sites/17/2021/02/bill-gates-e1613016156235.jpg",
+        text:
+            "“Tente uma, duas, três vezes e se possível tente a quarta, a quinta e quantas vezes for necessário. Só não desista nas primeiras tentativas, a persistência é amiga da conquista. Se você quer chegar aonde a maioria não chega, faça o que a maioria não faz”.",
+    },
+    {
+        id: 8,
+        name: "Elon Musk",
+        job: "CEO da Tesla",
+        img:
+            "https://img.olhardigital.com.br/wp-content/uploads/2021/01/shutterstock_1866780067-1.jpg",
+        text:
+            "“Quando algo é importante o suficiente, você faz, mesmo que as chances não estejam a seu favor”.",
+    },
+    {
+        id: 9,
+        name: "Jeff Bezos",
+        job: "CEO da Amazon",
+        img:
+            "https://i0.wp.com/assets.propmark.com.br/uploads/2019/09/jeff-bezos-2.jpg?resize=768%2C432&ssl=1",
+        text:
+            "“Se você é focado na competição, você terá que esperar até que um competidor faça algo. Se você é focado nos clientes, poderá ser pioneiro”.",
+    },
+    {
+        id: 10,
+        name: "Jack Ma",
+        job: "Fundador da Alibaba",
+        img:
+            "https://s2.glbimg.com/Q-vFS5ZXA5HzbIzx7QpBQZZS4WY=/0x0:1353x1789/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2020/d/D/SxACvqTh2WAHb1884S8w/foto29emp-101-alibaba-b7.jpg",
+        text:
+            "“O que é a falha? Desistir é o maior fracasso”.",
     },
 ];
 
